7244b25d467320733700b7dd75b9c6411777932e
itamarKraitman <itamar.kraitman@msmail.ariel.ac.il>
2023-09-11 04:05:53 +0300

    writing to screen correctly

--- Button.py
+++ Button.py
@@ -10,6 +10,16 @@ class Button:
 
     def draw(self, img, color: tuple):
         cv2.rectangle(img=img, pt1=self.luPos, pt2=self.drPos, color=color, thickness=3)
-        cv2.putText(img=img, text=self.text, org=self.textPos, fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=5,
+        if len(self.text) == 1:
+            cv2.putText(img=img, text=self.text, org=self.textPos, fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=5,
+                        color=(0, 0, 0), thickness=3)
+        else: # Delete button
+            cv2.putText(img=img, text=self.text, org=self.textPos, fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2,
+                        color=(0, 0, 0), thickness=3)
+        return img
+
+    def draw_with_external_text(self, img, color: tuple, text: str):
+        cv2.rectangle(img=img, pt1=self.luPos, pt2=self.drPos, color=color, thickness=3)
+        cv2.putText(img=img, text=text, org=self.textPos, fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=5,
                     color=(0, 0, 0), thickness=3)
         return img
--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import cv2
+import numpy as np
 from cvzone.HandTrackingModule import HandDetector
 from itertools import chain
 from Button import Button
 
 
 def create_buttons():
-    button_positions = [[(50 + i * 125, 350, 150 + i * 125, 450) for i in range(9)],
-                        [(50 + i * 125, 475, 150 + i * 125, 575) for i in range(9)],
-                        [(50 + i * 125, 600, 150 + i * 125, 700) for i in range(9)]]
+    button_positions = [[(50 + i * 125, 150, 150 + i * 125, 250) for i in range(9)],
+                        [(50 + i * 125, 275, 150 + i * 125, 375) for i in range(9)],
+                        [(50 + i * 125, 400, 150 + i * 125, 500) for i in range(10)]]
     button_text = [["A", "B", "C", "D", "E", "F", "G", "H", "I"],
                    ["J", "k", "L", "M", "N", "O", "p", "Q", "R"],
-                   ["S", "T", "U", "V", "W", "X", "Y", "Z"]]
-    text_position = [[(75 + i * 125, 430) for i in range(9)],
-                     [(75 + i * 125, 555) for i in range(9)],
-                     [(75 + i * 125, 680) for i in range(9)]]
+                   ["S", "T", "U", "V", "W", "X", "Y", "Z", "_", "Del"]]
+    text_position = [[(75 + i * 125, 230) for i in range(9)],
+                     [(75 + i * 125, 355) for i in range(9)],
+                     [(75 + i * 125, 480) for i in range(10)]]
     buttons = []
 
     for i in range(3):
@@ -21,18 +22,37 @@ def create_buttons():
                 position, text, t_position in zip(button_positions[i], button_text[i], text_position[i])]
         buttons.append(line)
 
-    return list(chain.from_iterable(buttons))
+    # Output button
+    output_button = Button(lu_pos=(50, 525), dr_pos=(1150, 625), text="", text_pos=(65, 600))
+
+    return list(chain.from_iterable(buttons)), output_button
 
 
-def hovering_over_button(img, buttons: list, lm: list):
+def hovering_over_button(detector: HandDetector, img, buttons: list, lm: list,
+                         final_output: str):
     for button in buttons:
         x_lu, y_lu = button.luPos
         x_dr, y_dr = button.drPos
-        print(x_lu, y_lu, x_dr, y_dr)
-        print(lm[8])
         if x_lu < lm[8][0] < x_dr and y_lu < lm[8][1] < y_dr:
-            print("yes")
             button.draw(img=img, color=(0, 255, 0, cv2.FILLED))
+            if click(detector=detector, img=img, lm=lm):
+                ru_pos, ld_pos = [button.drPos[0], button.luPos[1]], [button.luPos[0], button.drPos[1]]
+                points = np.array([button.luPos, ru_pos, button.drPos, ld_pos])
+                cv2.fillPoly(img=img, pts=[points], color=(0, 255, 0))
+                cv2.putText(img=img, text=button.text, org=button.textPos, fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=5,
+                            color=(0, 0, 0), thickness=3)
+                print(button.text)
+                final_output = final_output + ' ' if button.text == '_' else '' if button.text == 'Del' else final_output + button.text
+                print(final_output)
+
+    return final_output
+
+
+def click(detector: HandDetector, img, lm: list):
+    if detector.findDistance(p1=lm[8][:2], p2=lm[12][:2], img=img)[0] < 30:
+        print(lm[8][:2], lm[12][:2])
+        print("Click!")
+        return True
 
 
 def main():
@@ -42,6 +62,7 @@ def main():
     cap.set(4, 720)
 
     detector = HandDetector(detectionCon=0.8)
+    final_output = ""
 
     while True:
 
@@ -50,9 +71,10 @@ def main():
         hands, img = detector.findHands(img)
 
         # Drawing buttons
-        buttons = create_buttons()
+        buttons, output_button = create_buttons()
         for button in buttons:
             button.draw(img=img, color=(0, 0, 255, cv2.FILLED))
+        output_button.draw(img=img, color=(0, 0, 255, cv2.FILLED))
 
         if hands:
             # finding hands
@@ -61,8 +83,11 @@ def main():
 
             # Hovering over buttons
             if lm:
-                hovering_over_button(img=img, buttons=buttons, lm=lm)
+                final_output = hovering_over_button(detector=detector, img=img, buttons=buttons,
+                                                    lm=lm, final_output=final_output)
 
+        # Drawing output on screen
+        output_button.draw_with_external_text(img=img, color=(0, 0, 255, cv2.FILLED), text=final_output)
 
         cv2.imshow("Virtual Keyboard", img)
         cv2.waitKey(1)
